c3ba950edf1d6c6fe561f898c5a9a4d9105d7742
DanLevyM <dan.levy.pro@gmail.com>
2023-01-17 19:13:00 +0100

    vue - add bootstrap

--- vite-vue/src/components/TheNavigation.vue
+++ vite-vue/src/components/TheNavigation.vue
@@ -1,15 +1,23 @@
 <template>
-    <nav id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link :to="{ name: 'demo.show', params: { id: '1' } }"
-            >Demo1</router-link
-        >
-        <router-link :to="{ name: 'demo.show', params: { id: '2' } }"
-            >Demo2</router-link
-        >
-        | <router-link to="/login">Login</router-link> |
-        <router-link to="/register">Register</router-link>
+    <nav id="nav" class="d-flex justify-content-between p-3 bg-light">
+        <div>
+            <router-link to="/" class="p-2 m-3">Home</router-link>
+            <router-link to="/about" class="p-2 m-3">About</router-link>
+            <router-link
+                :to="{ name: 'demo.show', params: { id: '1' } }"
+                class="p-2 m-3"
+                >Demo1</router-link
+            >
+            <router-link
+                :to="{ name: 'demo.show', params: { id: '2' } }"
+                class="p-2 m-3"
+                >Demo2</router-link
+            >
+        </div>
+        <div>
+            <router-link to="/login" class="p-2 m-3">Login</router-link>
+            <router-link to="/register" class="p-2 m-3">Register</router-link>
+        </div>
     </nav>
 </template>
 
