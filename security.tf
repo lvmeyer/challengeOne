319c2cceecda2b659277aa797da54aaeb12f40f8
DanLevyM <dan.levy.pro@gmail.com>
2023-02-12 16:22:31 +0100

    cicd/ try debug ssl

--- security.tf
+++ security.tf
@@ -11,6 +11,24 @@ resource "aws_security_group" "ingress-all-test" {
 
   ingress {
     description      = "HTTPS"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS"
     from_port        = 5173
     to_port          = 5173
     protocol         = "tcp"
