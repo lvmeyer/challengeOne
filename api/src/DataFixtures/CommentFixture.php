9ff4630643af4fb2cb8c2cc44446ef2122f705bd
OdessaCh <odessa.chesneau@gmail.com>
2023-02-13 19:29:26 +0100

    Add fixture comment

--- api/src/DataFixtures/CommentFixture.php
+++ api/src/DataFixtures/CommentFixture.php
@@ -25,6 +25,19 @@ class CommentFixture extends Fixture implements DependentFixtureInterface
         $manager->persist($comment);
 
         $manager->flush();
+
+        $user1 = $manager->getRepository(User::class)->findAll()[0];
+        $movie1 = $manager->getRepository(Movie::class)->findAll()[0];
+
+        $comment1 = new Comment();
+        $comment1->setTitle('Bon film');
+        $comment1->setDescription('J\'ai bien aimé... Vraiment bienzjkghzohezfze rtfyghujiktrdfyguhijouhgtyfrgyhuigytfrdyguhijyftrdgyuhij');
+        $comment1->setDate(new \DateTime('2020-07-17'));
+        $comment1->setUserId($user1);
+        $comment1->setMovieId($movie1);
+        $manager->persist($comment1);
+
+        $manager->flush();
     }
 
     public function getDependencies()
